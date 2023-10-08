85b36507d198114ef8dda4c980ae878e48ac3590
DESKTOP-73172LO\szilveszter <nyesteszilveszter@gmail.com>
2023-10-08 15:57:25 +0200

    Bejelentkező oldal létrehozása

---
+++ App-frontend/app-frontend/src/components/login-page/Login.js
@@ -0,0 +1,45 @@
+// Login.js
+import React, { useState } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { login } from '../global-states/authSlice';
+
+function Login() {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const isLoggedIn = useSelector((state) => state.auth.isLoggedIn);
+  const dispatch = useDispatch();
+
+  const handleLogin = () => {
+    // You should perform your authentication logic here
+
+
+    // If authentication is successful, dispatch the login action
+    dispatch(login({ username, password }));
+  };
+
+  return (
+    <div>
+      {isLoggedIn ? (
+        <p>You are already logged in!</p>
+      ) : (
+        <div>
+          <input
+            type="text"
+            placeholder="Username"
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+          />
+          <input
+            type="password"
+            placeholder="Password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+          <button onClick={handleLogin}>Login</button>
+        </div>
+      )}
+    </div>
+  );
+}
+
+export default Login;
