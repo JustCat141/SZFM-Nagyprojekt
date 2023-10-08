b73d71760b59188724994af7bd1a35f5289a74f7
DESKTOP-73172LO\szilveszter <nyesteszilveszter@gmail.com>
2023-10-08 23:45:19 +0200

    Felhasználóhoz tartozó kérdőíveket listázó elemhez tartozó stílusok implementálása

--- App-frontend/app-frontend/src/components/dashboard/UserQuestionnairesList.js
+++ App-frontend/app-frontend/src/components/dashboard/UserQuestionnairesList.js
@@ -1,16 +1,19 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
+import { classes } from '../../styles/UserQuestionnairesList.module.css'
+
 
 const UserQuestionnairesList = () => {
     const UserQuestionnairesList = useSelector((state) => state.auth.UserQuestionnairesList);
     return (
-        <div>
-            <ul>
+        <div className={classes["UserQuestionnairesList-box"]}>
+            <ul className={classes["UserQuestionnairesList-list"]}>
             {
                 UserQuestionnairesList.forEach(questionnaire => {
                     return ( 
-                        <li key={questionnaire.id}>
-                            <div>
+                        <li key={questionnaire.id}
+                        className={classes["UserQuestionnairesList-list-item"]}>
+                            <div  className={classes["UserQuestionnairesList-list-item-text"]}>
                                 {questionnaire.title}
                             </div>
                         </li>
