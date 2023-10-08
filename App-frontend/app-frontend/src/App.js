457d70ddaad18dc1368ff96230735bc0577f5b24
DESKTOP-73172LO\szilveszter <nyesteszilveszter@gmail.com>
2023-10-08 15:59:18 +0200

    Globális állapotok és bejelentkező felület kezdetleges implementálása

--- App-frontend/app-frontend/src/App.js
+++ App-frontend/app-frontend/src/App.js
@@ -1,24 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { Provider } from 'react-redux';
+import { configureStore } from '@reduxjs/toolkit';
+import authReducer from './components/global-states/authSlice';
+import Login from './components/login-page/Login'; // Import your Login component
+
+const store = configureStore({
+  reducer: {
+    auth: authReducer,
+  },
+});
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Provider store={store}>
+      {store.getState().auth.isLoggedIn ? (
+        <p>You are already logged in!</p>
+      ) : (
+        <Login />
+      )}
+    </Provider>
   );
 }
 
