170f2e7fe5e160fd6a41381bcf8bd61ac44513d5
DESKTOP-73172LO\szilveszter <nyesteszilveszter@gmail.com>
2023-10-10 20:06:34 +0200

    Az egy vagy több válaszlehetőség megkülönböztetésének implementálása

--- App-frontend/app-frontend/src/components/questionnairefill/QuestionnaireFill.js
+++ App-frontend/app-frontend/src/components/questionnairefill/QuestionnaireFill.js
@@ -1,21 +1,31 @@
 import { useSelector, useDispatch } from 'react-redux';
 import { useState } from 'react'; // Import useState
 import Card from "../helper-functions/Card.js";
-import "./fill.css";
+import './fill.css'; // Import the fill.css file
 
 const QuestionnaireFill = () => {
   const questionnaireData = useSelector((state) => state.auth.CurrentForFill);
   const questions = questionnaireData.quests;
 
-  // State to track the active answer for each question
-  const [activeAnswers, setActiveAnswers] = useState(new Array(questions.length).fill(null));
+  // State to track the active answers for each question
+  const [activeAnswers, setActiveAnswers] = useState(questions.map(() => []));
 
   const handleAnswerClick = (questionIndex, answerIndex) => {
     // Clone the current activeAnswers array
     const newActiveAnswers = [...activeAnswers];
     
-    // Set the clicked answer as active for the specified question
-    newActiveAnswers[questionIndex] = answerIndex;
+    // If the question type is 'one', replace the previous active answers
+    if (questions[questionIndex].type === 'one') {
+      newActiveAnswers[questionIndex] = [answerIndex];
+    } else if (questions[questionIndex].type === 'multiple') {
+      // If the question type is 'multiple', toggle the active answer
+      const answerIndexInArray = newActiveAnswers[questionIndex].indexOf(answerIndex);
+      if (answerIndexInArray === -1) {
+        newActiveAnswers[questionIndex].push(answerIndex); // Select the answer
+      } else {
+        newActiveAnswers[questionIndex].splice(answerIndexInArray, 1); // Deselect the answer
+      }
+    }
 
     // Update the state
     setActiveAnswers(newActiveAnswers);
@@ -40,7 +50,7 @@ const QuestionnaireFill = () => {
                     {question.answers.map((answer, answerIndex) => (
                       <li
                         key={answerIndex}
-                        className={answerIndex === activeAnswers[questionIndex] ? 'active' : ''}
+                        className={`answer ${activeAnswers[questionIndex].includes(answerIndex) ? 'active' : ''}`}
                         onClick={() => handleAnswerClick(questionIndex, answerIndex)} // Handle click
                       >
                         {answer}
