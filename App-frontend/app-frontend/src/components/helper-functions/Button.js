11a8fce8f534284c175aa72f16df2092eea6a76e
DESKTOP-73172LO\szilveszter <nyesteszilveszter@gmail.com>
2023-10-11 13:41:57 +0200

    Általános gomb stílusainak az implementálása

--- App-frontend/app-frontend/src/components/helper-functions/Button.js
+++ App-frontend/app-frontend/src/components/helper-functions/Button.js
@@ -1,4 +1,4 @@
-
+import "../../styles/Button.css";
 export const Button = props => {
     return <div className="button-box">
         <button className="button-text" onClick={props.func}>{props.text}</button>
