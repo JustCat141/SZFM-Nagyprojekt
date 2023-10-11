4586a8f0e74c8a3e8cf7c629087f3eaf1e1b3c24
DESKTOP-73172LO\szilveszter <nyesteszilveszter@gmail.com>
2023-10-11 20:30:04 +0200

    Input fieldek és gombok hozzáadása

--- App-frontend/app-frontend/src/components/questionnaireEditor/QuestionnaireEditor.js
+++ App-frontend/app-frontend/src/components/questionnaireEditor/QuestionnaireEditor.js
@@ -1,23 +1,42 @@
 import React, { useState } from "react";
 import QuestionList from "./QuestionList";
 import { Button } from "../helper-functions/Button";
+import Card from "../helper-functions/Card";
+import OptionList from "./OptionList";
 
 const QuestionnaireEditor = () => {
   const [questionList, setQuestionList] = useState([]);
-  const [newQuestionId, setNewQuestionId] = useState(1); // Keep track of the new question IDs
+  const [newQuestionId, setNewQuestionId] = useState(1);
+
+  const [questionTitle, setQuestionTitle] = useState("");
+  const [questionDescription, setQuestionDescription] = useState("");
+  const [optionList, setOptionList] = useState([]);
+  const [newOptionText, setNewOptionText] = useState("");
 
   const handleNewQuestion = () => {
-    // Create a new question with a unique ID and other properties
     const newQuestion = {
       id: newQuestionId,
-      text: "New Question", // Replace with the default text for new questions
+      title: questionTitle,
+      description: questionDescription,
+      type: "one", // Default type (you can change it)
+      answers: optionList, // Use the optionList as answers
     };
 
-    // Increment the new question ID for the next question
     setNewQuestionId(newQuestionId + 1);
-
-    // Add the new question to the question list
     setQuestionList([...questionList, newQuestion]);
+
+    // Reset the input fields for the next question
+    setQuestionTitle("");
+    setQuestionDescription("");
+    setOptionList([]);
+  };
+
+  const handleNewOption = () => {
+    // Add a new option to the optionList
+    if (newOptionText) {
+      setOptionList([...optionList, newOptionText]);
+      setNewOptionText("");
+    }
   };
 
   const handleSubmit = () => {
@@ -27,7 +46,31 @@ const QuestionnaireEditor = () => {
 
   return (
     <div>
+      <Card>
+        <input
+          name="title"
+          placeholder="Questionnaire Title"
+          value={questionTitle}
+          onChange={(e) => setQuestionTitle(e.target.value)}
+        />
+        <input
+          name="desc"
+          placeholder="Questionnaire Description"
+          value={questionDescription}
+          onChange={(e) => setQuestionDescription(e.target.value)}
+        />
+      </Card>
       <QuestionList questions={questionList} />
+      <Card>
+        <input
+          name="question"
+          placeholder="Question Text"
+          value={newOptionText}
+          onChange={(e) => setNewOptionText(e.target.value)}
+        />
+        <OptionList options={optionList} />
+        <Button text={"Új válaszlehetőség"} func={handleNewOption} />
+      </Card>
       <div>
         <Button text={"Új kérdés"} func={handleNewQuestion} />
         <Button text={"Kérdőív mentése"} func={handleSubmit} />
