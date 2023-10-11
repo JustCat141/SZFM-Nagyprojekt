51068b066f448dba313bd6b99d738b06a14ad620
DESKTOP-73172LO\szilveszter <nyesteszilveszter@gmail.com>
2023-10-11 21:04:16 +0200

    Kérdések listájának a stílusainak struktúrájának implementálása

--- App-frontend/app-frontend/src/components/questionnaireEditor/QuestionList.js
+++ App-frontend/app-frontend/src/components/questionnaireEditor/QuestionList.js
@@ -1,24 +1,23 @@
 import Card from "../helper-functions/Card";
 import OptionList from "./OptionList";
-
+import classes from "../../styles/QuestionList.module.css";
 const QuestionList = (props) => {
     const questions = props.questions;
-    return <>
+    return <div className={classes["question-list-box"]}>
         {
             questions.map((question, index) => {
                 return (
                     <Card key={index} >
-                        <div style={{border : "thin solid red"}}>
-                            <div>Kérdés: {question.title}</div>
-                            <div>Leírás: {question.description}</div>
+                        <div className={classes["question-box"]}  style={{border : "thin solid red"}}>
+                            <div className={classes["question-text"]}>Kérdés: {question.title}</div>
+                            <div className={classes["question-text"]}>Leírás: {question.description}</div>
                             <OptionList options={question.quests}/>
                         </div>
-                    
                     </Card>
                 )
             })
         }
-        </>;
+        </div>;
 };
 
 export default QuestionList;
