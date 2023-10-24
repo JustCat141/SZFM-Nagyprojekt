f822c83a9f98d2f7760f14dd068eb43bd5c9220f
Somogyi Patrik <patreszkaa@gmail.com>
2023-10-25 00:42:25 +0200

    Backend - Új response típus felvétele

--- backend/api/src/Helpers/ResponseHelper.js
+++ backend/api/src/Helpers/ResponseHelper.js
@@ -1,5 +1,6 @@
 export const success = { success: true, message: null }
 export const invalidEmailFormat = { success: false, message: "Invalid email format!" }
 export const invalidPasswordFormat = { success: false, message: "Invalid password format!" }
-export const emailAddressTaken = { success: false, message: "Email address already taken" }
+export const emailAddressTaken = { success: false, message: "Email address already taken!" }
 export const invalidLogin = { success: false, message: "Invalid email or password!" }
+export const invalidFormat = { success: false, message: "Invalid data structure!" }
