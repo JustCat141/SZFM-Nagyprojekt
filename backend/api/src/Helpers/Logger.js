2e4c7cdce48f13582935f83591e94384102e4302
Somogyi Patrik <patreszkaa@gmail.com>
2023-11-02 17:52:57 +0100

    Backend - Kisebb módosítás a Logger kiírásának szerkezetében

--- backend/api/src/Helpers/Logger.js
+++ backend/api/src/Helpers/Logger.js
@@ -1,42 +1,41 @@
 import chalk from 'chalk'
 
-export function logger() {
-    return new Logger();
-}
-
-export class Logger {
+class Logger {
     constructor(ignoredLogLevel) {
         this.ignoredLogLevel = ignoredLogLevel
         this.logTypes = {
-            Error: chalk.red,
-            Warning: chalk.yellow,
-            Info: chalk.blueBright,
-            Debug: chalk.green,
+            ERROR: chalk.red,
+            WARNING: chalk.yellow,
+            INFO: chalk.blueBright,
+            DEBUG: chalk.green,
         };
     }
 
     #log(type, message) {
         if (this.logTypes[type]) {
             const coloredType = this.logTypes[type](type);
-            console.log(`[${coloredType}] ${message}`);
+            const currentTime = chalk.gray(`(${new Date().toLocaleTimeString()})`)
+            console.log(`${currentTime} [${coloredType}] ${message}`);
         } else {
             console.log(message);
         }
     }
 
     error(message) {
-        this.#log('Error', message);
+        this.#log('ERROR', message);
     }
 
     warn(message) {
-        this.#log('Warning', message);
+        this.#log('WARNING', message);
     }
 
     info(message) {
-        this.#log('Info', message);
+        this.#log('INFO', message);
     }
 
     debug(message) {
-        this.#log('Debug', message);
+        this.#log('DEBUG', message);
     }
 }
+
+export const logger = new Logger();
