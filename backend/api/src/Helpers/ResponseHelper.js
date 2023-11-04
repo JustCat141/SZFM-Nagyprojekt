f7957844595930531f723071433f43f8f5f37610
Somogyi Patrik <patreszkaa@gmail.com>
2023-11-04 12:35:10 +0100

    Backend - Szöveg kicserélése hibakódokra

--- backend/api/src/Helpers/ResponseHelper.js
+++ backend/api/src/Helpers/ResponseHelper.js
@@ -1,6 +1,6 @@
-export const success = { success: true, message: null }
-export const invalidEmailFormat = { success: false, message: "Invalid email format!" }
-export const invalidPasswordFormat = { success: false, message: "Invalid password format!" }
-export const emailAddressTaken = { success: false, message: "Email address already taken!" }
-export const invalidLogin = { success: false, message: "Invalid email or password!" }
-export const invalidFormat = { success: false, message: "Invalid data structure!" }
+export const success = { success: true, errorcode: null }
+export const invalidEmailFormat = { success: false, errorcode: 101 }
+export const invalidPasswordFormat = { success: false, errorcode: 102 }
+export const invalidLogin = { success: false, errorcode: 103 }
+export const invalidFormat = { success: false, errorcode: 104 }
+export const emailAddressTaken = { success: false, errorcode: 201 }
